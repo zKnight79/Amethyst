9341440a1c6d7b8b50c24ee48b846b4ec2d895f0
zKnight79 <guillaume.jibaut@live.com>
2017-02-07 21:36:01 +0100

    Bouncing Ball Sample

--- Samples/S02_BouncingBall/BouncingBallGame.cs
+++ Samples/S02_BouncingBall/BouncingBallGame.cs
@@ -32,17 +32,40 @@ namespace S02_BouncingBall
         }
         protected override void OnRelease()
         {
-            ballTexture?.Dispose();
+            ballTexture.Dispose();
         }
 
+        private float rotationSpeed = 360; // (degrees per second)
+        private float xAxisSpeed = 300;    // (pixels per second)
+        private float yAxisSpeed = 240;    // (pixels per second)
         protected override void OnUpdate(float elapsedTime)
         {
-            ballSprite.Angle += elapsedTime * 360;
-            ballSprite?.Update(elapsedTime);
+            // Apply rotation speed
+            ballSprite.Angle += elapsedTime * rotationSpeed;
+
+            // Apply x-axis speed
+            ballSprite.BoxX += elapsedTime * xAxisSpeed;
+            if((xAxisSpeed<0 && ballSprite.BoxLeft<=ViewPort.Left)
+                ||
+               (xAxisSpeed > 0 && ballSprite.BoxRight >= ViewPort.Right))
+            {
+                xAxisSpeed = -xAxisSpeed;
+            }
+
+            // Apply y-axis speed
+            ballSprite.BoxY += elapsedTime * yAxisSpeed;
+            if ((yAxisSpeed < 0 && ballSprite.BoxTop <= ViewPort.Top)
+                ||
+               (yAxisSpeed > 0 && ballSprite.BoxBottom >= ViewPort.Bottom))
+            {
+                yAxisSpeed = -yAxisSpeed;
+            }
+
+            ballSprite.Update(elapsedTime);
         }
         protected override void OnRender(SpriteBatch spriteBatch)
         {
-            ballSprite?.Render(spriteBatch);
+            ballSprite.Render(spriteBatch);
             spriteBatch.DrawText("FPS : " + GameTime.FramesPerSecond, SystemFont, TextRenderMode.Inline, ViewPort, Color4.Colors.White);
         }
     }
